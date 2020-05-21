79c5c978a71df56df82ea1c9bc57ff7a1d7a09a4
tk-ozawa <ozawa.shotaro0114@gmail.com>
2020-05-21 14:03:37 +0900

    add: user情報登録/出力機能実装

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,11 +1,18 @@
-import { Controller, Post, Body } from '@nestjs/common';
+import { Controller, Get, Post, Body } from '@nestjs/common';
 import { CreateUserDto } from './dto/create-user.dto'
+import { UsersService } from './users.service';
 
 @Controller('users')
 export class UsersController {
+  constructor(private readonly usersService: UsersService) { }
+
+  @Get()
+  findAll() {
+    return this.usersService.findAll()
+  }
 
   @Post()
   creat(@Body() createUsers: CreateUserDto) {
-    return createUsers
+    return this.usersService.create(createUsers)
   }
 }
