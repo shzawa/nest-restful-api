8f91594a1fdcdb679574c900f61ff60eb8eaf0d6
tk-ozawa <ozawa.shotaro0114@gmail.com>
2020-05-21 13:28:51 +0900

    add: サーバ起動時にルーティング一覧を表示するようにした

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,20 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import * as listEndpoints from "express-list-endpoints";
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   await app.listen(3000);
+
+  const server = app.getHttpServer();
+  const router = server._events.request._router;
+  const endpoints = listEndpoints(router);
+  endpoints.forEach(endpoint => {
+    console.log(
+      `${endpoint.methods.filter((v, i, a) => a.indexOf(v) === i)} ${
+      endpoint.path
+      }`
+    );
+  });
 }
 bootstrap();
