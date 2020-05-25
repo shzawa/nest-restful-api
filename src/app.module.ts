b006b71ce0fbef176bd77d4deaa7dff868299608
tk-ozawa <ozawa.shotaro0114@gmail.com>
2020-05-25 14:49:41 +0900

    update: MongoDBAtlasを利用

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,8 +4,13 @@ import { AppService } from './app.service';
 import { UsersModule } from './users/users.module';
 import { MongooseModule } from '@nestjs/mongoose';
 
+require('dotenv').config()
+
 @Module({
-  imports: [UsersModule, MongooseModule.forRoot('mongodb://localhost:27017/test')],
+  imports: [
+    UsersModule,
+    MongooseModule.forRoot(`mongodb+srv://${process.env.DB_USERNAME}:${process.env.DB_PASSWORD}@cluster0-lp6cn.gcp.mongodb.net/${process.env.DB_NAME}?retryWrites=true&w=majority`)
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
