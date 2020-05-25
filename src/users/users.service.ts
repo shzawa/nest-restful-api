041dad90c11f0a2a4014adba68e7f5ab29cb8ee2
tk-ozawa <ozawa.shotaro0114@gmail.com>
2020-05-25 14:52:01 +0900

    add: Userモデルを使ったユーザー登録

--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -1,12 +1,23 @@
 import { Injectable } from '@nestjs/common';
 import { CreateUserDto } from './dto/create-user.dto';
+import { Model } from 'mongoose';
+import { User } from './interfaces/user.interface';
+import { InjectModel } from '@nestjs/mongoose';
 
 @Injectable()
 export class UsersService {
+  constructor(@InjectModel('User') private readonly userModel: Model<User>) {
+  }
+
+
   users: CreateUserDto[] = []
 
-  create(user: CreateUserDto) {
-    this.users.push(user)
+  async create(user: CreateUserDto) {
+    const createdUser = new this.userModel({
+      username: user.username,
+      password: user.password
+    })
+    return await createdUser.save()
   }
 
   findAll() {
