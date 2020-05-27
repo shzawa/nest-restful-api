8f59ee0b95bff3981d28ffb79ebbb9f2ec20dd5a
tk-ozawa <ozawa.shotaro0114@gmail.com>
2020-05-27 20:53:13 +0900

    update: AuthModuleでUsersModuleとJwtModuleをimport

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -1,9 +1,18 @@
 import { Module } from '@nestjs/common';
 import { AuthController } from './auth.controller';
 import { AuthService } from './auth.service';
+import { JwtModule } from "@nestjs/jwt"
+import { UsersModule } from '../users/users.module';
 
 @Module({
+  imports: [
+    UsersModule,
+    JwtModule.register({
+      secret: 'secret',  // 本番ではランダム文字列に
+      signOptions: { expiresIn: '1h' }
+    })
+  ],
   controllers: [AuthController],
   providers: [AuthService]
 })
-export class AuthModule {}
+export class AuthModule { }
