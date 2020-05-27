4b98f3d37872db73cf1be30428e87130093751f6
tk-ozawa <ozawa.shotaro0114@gmail.com>
2020-05-27 20:52:32 +0900

    add: signIn用アクション追加

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,4 +1,13 @@
-import { Controller } from '@nestjs/common';
+import { Controller, ValidationPipe, Post, Body } from '@nestjs/common';
+import { AuthService } from './auth.service';
+import { CreateUserDto } from '../users/dto/create-user.dto';
 
 @Controller('auth')
-export class AuthController {}
+export class AuthController {
+  constructor(private readonly authService: AuthService) { }
+
+  @Post('sign_in')
+  creat(@Body(ValidationPipe) createUsers: CreateUserDto) {
+    return this.authService.login(createUsers)
+  }
+}
